3c88a788119ce547a205d1f255d533dcc698510e
Eduard1818 <eduardotumimbo.yns@gmail.com>
2022-04-12 17:43:53 +0800

    Initial Microblog Homepage

---
+++ resources/views/partials/layout.blade.php
@@ -0,0 +1,12 @@
+@include('partials.header');
+@include('partials.navbar'); 
+
+
+<div class="main-content">
+  
+    @yield('content')
+
+</div>
+
+
+@include('partials.footer');
---
+++ resources/views/partials/navbar.blade.php
@@ -0,0 +1,31 @@
+<div class="container">
+    <nav class="navbar navbar-expand-md navbar-dark fixed-top bg-dark ">
+        <div class="container-fluid">
+            <a class="navbar-brand" href="#">YNS</a>
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarCollapse" aria-controls="navbarCollapse" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarCollapse">
+                <ul class="navbar-nav ms-auto mb-2 mb-md-0">
+                    <li class="nav-item">
+                        <a class="nav-link active" aria-current="page" href="#">Home</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link active" aria-current="page" href="#">About Us</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link active" aria-current="page" href="#">Contact Us</a>
+                    </li>
+                </ul>
+
+                <form class="d-flex">
+                    <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
+                    <button class="btn btn-dark" type="submit"><i class="bi bi-search"></i></button>
+                    
+                    <button class="btn btn-dark" type="submit">Login</button>
+                    <button class="btn btn-dark" type="submit">Register</button>
+                </form>
+            </div>
+        </div>
+    </nav>
+</div>
